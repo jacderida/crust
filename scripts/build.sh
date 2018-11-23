8b85e949176dd96420f62026de53db309c9a6aef
Chris O'Neil <chris.oneil@gmail.com>
2018-11-23 14:51:29 +0000

    chore/ci: re-enable volume

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,8 @@ set -x;
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
     cargo test --release --verbose
 elif [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+    echo $PWD
+    ls -al
     version=$(cat Cargo.toml | grep "^version" | awk '{ print $3 }' | sed 's/\"//g')
-    docker run --rm maidsafe/crust:$version
+    docker run --rm -v "$PWD:/usr/src/crust" maidsafe/crust:$version
 fi
